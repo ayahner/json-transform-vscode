6801ee965bcccfb1bb644f66006889d29073db39
Andrew Yahner <andrew.yahner@acadiasoft.com>
2021-12-02 15:30:25 -0500

    Installed basic first test samples

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,6 @@ import * as vscode from 'vscode';
 
 import * as panel from './OutputPanel';
 
-console.log('json-transform-vscode: extension "json-transform-vscode" is instantiatedˇ');
-
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
