a24480848b33c89a74099ed29fa6d45fa3a70d65
Andrew Yahner <andrew.yahner@acadiasoft.com>
2021-12-02 15:04:38 -0500

    Added Basic project stricture experimenting with api

---
+++ src/OutputPanel.ts
@@ -0,0 +1,9 @@
+export function openPanel() {
+    console.log('json-transform-vscode.OutputPanel: openPanel() called');
+
+}
+
+export function closePanel() {
+    console.log('json-transform-vscode.OutputPanel: closePanel() called');
+
+}
